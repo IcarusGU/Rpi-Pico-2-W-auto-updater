3dc25ce0f62786120a85b4d7987e94fc881ea4b4
Levi <the4thmcguire@gmail.com>
2026-02-05 02:28:05 -0800

    Added autoupdater.py logic

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ In many cases it will be:
 ../repo name
 
 ## Use
+*NOTE*
+
 
 Import autoupdater. Then, use autoupdater.checkUpdates() or autoupdater.Update(target). checkUpdates() returns true or false depending on if there is an update, and Update(target) returns 0 or 1 depending on if the update has been succesful. 
 
-If using an SD card, call UpdateSD(target)
+If using an SD card, call Update(target) where target contains the mounting path to your sd card.
 
 ## Setup instructions
 
@@ -22,9 +24,17 @@ https://raw.githubusercontent.com/IcarusGU/Pico2w-autoupdater/main/
 
 Or, if you wish to only use a certain folder within the repo:
 
-https://raw.githubusercontent.com/IcarusGU/Pico2w-autoupdater/main/desiredFolder
+https://raw.githubusercontent.com/IcarusGU/Pico2w-autoupdater/main/desiredFolder/
 
-Ensure that there is a top level file named version.txt within the git repo you are updating from.
+Ensure that there is a file named version.txt within the git repo you are updating from, and that the file contains the version number on the first line. Additionally, version.txt must be top level within whatever folder you are pathing to.
 
 If it is desired, you may connect to wifi for this process by putting the desired wifi information within wifi.txt
+Put the wifi ssid on the first line, and wifi password on the second.
 Otherwise, it is assumed you are already connected to wifi.
+If you do not wish to use this, ensure that wifi.txt is left blank.
+
+### Dependencies
+
+The autoupdater assumes urequests and network are preinstalled on the pico, as they are with most micropython installs. If they are not, please install them.
+
+Additionally, for sd card support you must install the sdcard library.
--- autoupdater.py
+++ autoupdater.py
@@ -1,1 +1,132 @@
+import urequests
+import network
+import time
+import os
+import gc
 
+#Consts
+headers = {'User-Agent': 'Pico2W'}
+
+#Helpers
+def getFirstLine(file):
+    try:
+        with open(file, "r") as f:
+            line = f.readline().strip()
+            return line
+    except OSError:
+        return None
+
+def getAPIurl(url):
+    parts = url.split('/')
+    user = parts[3]
+    repo = parts[4]
+    branch = parts[5]
+    return f"https://api.github.com/repos/{user}/{repo}/git/trees/{branch}?recursive=1"
+
+def currentInstall():
+    line = getFirstLine("version.txt")
+    version = ''.join(char for char in line if char.isdigit())
+    return version
+
+def newestVersion():
+    try:
+        path = getFirstLine("path.txt") + "version.txt"
+        response = urequests.get(path)
+        first_line = response.raw.readline().decode('utf-8').strip()
+        response.close()
+        version = ''.join(char for char in first_line if char.isdigit())
+        return version
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        return None
+    
+def rm_rf(target):
+    try:
+        items = os.listdir(target)
+    except OSError:
+        print("Target invalid!")
+        return
+
+    for item in items:
+        full_path = f"{target}/{item}"
+        
+        try:
+            os.remove(full_path)
+            print(f"Deleted file: {full_path}")
+        except OSError:
+            rm_rf(full_path)
+
+    os.rmdir(target)
+    print(f"Deleted folder: {target}")
+
+#Main functions
+def checkUpdates():
+    if currentInstall() < newestVersion():
+        return True
+    return False
+
+def Update(target):
+    path = getFirstLine("path.txt")
+    apiUrl = getAPIurl(path)
+    try: os.mkdir(target)
+    except: pass
+    try:
+        rm_rf(target)
+
+        try:
+            r = urequests.get(apiUrl, headers=headers)
+            data = r.json()
+            r.close()
+        except Exception as e:
+            print(f"API Error: ", e)
+            return
+        
+        for item in data['tree']:
+            filePath = item['path']
+            fileType = item['type']
+            
+            locPath = f"{target}/{filePath}"
+            if fileType == "tree":
+                try:
+                    os.mkdir(locPath)
+                    print(f"Created Dir: {locPath}")
+                except:
+                    pass
+                    
+            elif fileType == "blob":
+                rawUrl = path + filePath
+                try:
+                    res = urequests.get(rawUrl, stream=True)
+                    with open(locPath, "wb") as f:
+                        while True:
+                            chunk = res.raw.read(512)
+                            if not chunk:
+                                break
+                            f.write(chunk)
+                except Exception as e:
+                    print(f"Failed to download {locPath}: ", e)
+                gc.collect()
+    except OSError as e:
+        print("Failed to update: ", e)
+
+#Wifi support
+def wifiAttempt(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    while wlan.status() != 3:
+        print('Connecting to ssid: ', ssid)
+        time.sleep(1)
+    print('Connected')
+    status = wlan.ifconfig()
+    print('IP Address = ' + status[0])
+
+try:
+    with open("wifi.txt", "r") as f:
+        ssid = f.readline().strip()
+        if len(ssid) > 0:
+            password = f.readline().strip()
+            wifiAttempt(ssid, password)
+except OSError:
+    print("Could not connect to wifi")
